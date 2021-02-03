80ddba19e63dac72e2a58ce82f3116b833edebae
aldo ferdiansyah <ferdiansyahaldo@icloud.com>
2021-02-04 00:57:48 +0700

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Portofolio</h1>
-      </header>
-    </div>
+    <Navbar/>
   );
 }
 
